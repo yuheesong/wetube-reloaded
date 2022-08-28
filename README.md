aadddae2244ac5d96c032ef282134567b01b1441
yuheesong <thddbgml345@naver.com>
2022-08-28 13:13:56 +0900

    [templates] Configuring Pug

---
+++ README.md
@@ -0,0 +1,15 @@
+#Wetube Reloaded
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See user
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile
+
+/videos/:id -> See Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/Upload -> Upload Video
---
+++ src/controllers/videoController.js
@@ -0,0 +1,16 @@
+export const trending = (req, res) => 
+res.render("home");
+export const see = (req, res) => {
+  return res.send(
+    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy; 2021 Wetube</footer></body></html> `
+    );
+
+};
+export const edit = (req, res) => {
+  return res.send("Edit");
+};
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+  return res.send("Delete Video");
+};
